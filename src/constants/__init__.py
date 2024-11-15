2d02d7d2db6da4e3d1334de839feb14fceec8710
pheonix2109 <pallavijagre123@gmail.com>
2024-11-16 03:47:41 +0530

    data transformation

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -23,3 +23,17 @@ RAW_DATA_DIR_KEY = "raw.csv"
 TRAIN_DATA_DIR_KEY ="train.csv"
 TEST_DATA_DIR_KEY = "test.csv"
 
+
+
+# DaTA Transformation Related Variable
+
+DATA_TRANSFORMATION_ARTIFACT = "data_transformation"
+DATA_PREPROCESSED_DIR = "processor"
+DATA_TRANSFORMATION_PROCESSING_OBJ = "processor.pkl"
+DATA_TRANSFORM_DIR = "transformation"
+TRANSFORM_TRAIN_DIR_KEY = "train.csv"
+TRANSFORM_TEST_DIR_KEY = "test.csv"
+
+# artifact / data_transformation / processor -> processor.pkl and transformation -> train.csv and test.csv
+
+
